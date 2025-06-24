38b64b2ea86e2a609ea8b8a43349c63924472ba8
jugulde03 <kabriacid01@gmail.com>
2025-06-24 04:59:19 +0100

    Update README with future feature enhancements and modify header component for improved icon and stylesheet management

--- components/header.php
+++ components/header.php
@@ -14,21 +14,10 @@ function set_title(string $title = 'Water Management System')
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Page title is set dynamically -->
     <title><?= set_title('Water Management System' ?? null) ?></title>
-    <link rel="shortcut icon" href="../favicon.png" type="image/x-icon">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap-grid.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap-utilities.min.css">
-
+    <link rel="icon" href="/water-supply-system/assets/img/logo.png" type="image/x-icon">
     <!-- Font Awesome for icons -->
     <link href="https://demos.creative-tim.com/argon-dashboard-pro/assets/css/nucleo-svg.css" rel="stylesheet">
     <link href="https://demos.creative-tim.com/argon-dashboard-pro/assets/css/nucleo-icons.css" rel="stylesheet">
-
-    <!-- Lottie Animations -->
-    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-    <!-- <script src="../assets/js/lottie-player.js"></script> -->
-
-    <!-- Toasted JS for notifications -->
-    <link rel="stylesheet" href="../assets/css/toasted.css" />
-    <script src="../assets/js/toasted.js"></script>
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Sora:wght@100..800&display=swap">
 
